568f1e0604d40b7019f6ba3054ecaab628cf3975
homedirect1 <homedirect.tech@gmail.com>
2022-03-24 12:42:37 +0530

    add

---
+++ app/etc/env.php
@@ -0,0 +1,79 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '6db1abace12bcea7d0619a74e75c072c'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'homedirect_db',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'production',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '7aa_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '7aa_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Fri, 29 Oct 2021 10:33:23 +0000'
+    ]
+];
